63455aac129d247d70760936f6cf2c6b23dc3519
Reem Osama <reem.osama3052001@gmail.com>
2023-02-01 14:10:51 +0200

    add circular image using circleAvatar

---
+++ lib/layout/card_home.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+
+class BusinessCardHome extends StatelessWidget {
+  const BusinessCardHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+
+
+        children:   [
+          CircleAvatar(
+            backgroundColor: Colors.black,
+            radius: 122,
+            child: CircleAvatar(
+              radius: 120, // Image radius
+              backgroundImage:AssetImage("lib/assets/image/card.png")
+              ),
+          )
+
+
+        ],
+      ),
+
+    );
+  }
+}
